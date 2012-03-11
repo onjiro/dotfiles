e9bc95393d612e5fd3ca8723e1e75bf862c4c4fc
onjiro <onjiro.mohyahya@gmail.com>
2012-03-11 14:55:43 +0900

    use absolute path when setup

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ DOT_FILES=( .profile .zshrc .gitconfig .gitignore .emacs .emacs.d .hgrc )
 
 for file in ${DOT_FILES[@]}
 do
-    ln -s $(dirname $0)/$file $HOME/$file
+    ln -s $(cd $(dirname $0) && pwd)/$file $HOME/$file
 done
