4509a53d1a5a248a369ba7753c030fcb15777a73
onjiro <onjiro.mohyahya@gmail.com>
2013-08-12 00:15:17 +0900

    Revert "Extract ruby-mode settings into "21-ruby.el""
    This reverts commit 3a3c3664fcd8bfce2783a0f20c40fd140ee832c3.
    
    Conflicts:
    
    	.emacs.d/init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -24,6 +24,30 @@
 (add-to-list 'default-frame-alist '(width . 120))
 (add-to-list 'default-frame-alist '(height . 40))
 
+;;; ruby関連の設定
+(autoload 'ruby-mode "ruby-mode"
+  "Mode for editing ruby source files" t)
+(setq auto-mode-alist
+      (append '(("\\.rb$" . ruby-mode)) auto-mode-alist))
+(setq interpreter-mode-alist (append '(("ruby" . ruby-mode))
+                                     interpreter-mode-alist))
+(autoload 'run-ruby "inf-ruby"
+  "Run an inferior Ruby process")
+(autoload 'inf-ruby-keys "inf-ruby"
+  "Set local key defs for inf-ruby in ruby-mode")
+(add-hook 'ruby-mode-hook
+          '(lambda () (inf-ruby-keys)))
+
+;; ruby-electric
+(require 'ruby-electric)
+(add-hook 'ruby-mode-hook '(lambda () (ruby-electric-mode t)))
+
+;; ruby-block
+(require 'ruby-block)
+(ruby-block-mode t)
+;; ミニバッファに表示し, かつ, オーバレイする.
+(setq ruby-block-highlight-toggle t)
+
 ;; capitalize を backword で適用
 (global-set-key "\M-C" 'capitalize-backward-word)
 (defun capitalize-backward-word (arg)
