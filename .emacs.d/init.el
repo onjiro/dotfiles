acc63711377ad5333031d1b742c67e04f59ef50f
onjiro <onjiro.mohyahya@gmail.com>
2013-08-12 00:06:19 +0900

    Extract scala-mode settings into "21-scala.el"

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -24,23 +24,6 @@
 (add-to-list 'default-frame-alist '(width . 120))
 (add-to-list 'default-frame-alist '(height . 40))
 
-;;; scalaモード用にロードパスを追加
-(add-to-list 'load-path "~/.emacs.d/scala-mode")
-    (require 'scala-mode-auto)
-
-;; Load the ensime lisp code...
-;;(add-to-list 'load-path "~/etc/ensime/elisp/")
-;;(require 'ensime)
-
-;; This step causes the ensime-mode to be started whenever
-;; scala-mode is started for a buffer. You may have to customize this step
-;; if you're not using the standard scala mode.
-;;(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
-
-;; MINI HOWTO:
-;; Open .scala file. M-x ensime (once per project)
-(put 'scroll-left 'disabled nil)
-
 ;; capitalize を backword で適用
 (global-set-key "\M-C" 'capitalize-backward-word)
 (defun capitalize-backward-word (arg)
