1dd697aeec818b5a9976f6aa22edcb9d5a2e8284
onjiro <onjiro.mohyahya@gmail.com>
2013-08-12 00:42:35 +0900

    Reverting "Extract coffee-mode settings into "21-coffee.el"" Revert "Extract coffee-mode settings into "21-coffee.el""
    This reverts commit 0d326d115cc49084340c53b3225a286b1af0a309.

--- .emacs.d/inits/21-coffee.el
+++
@@ -1,5 +0,0 @@
-;; coffee mode
-(add-to-list 'load-path "~/.emacs.d/vendor/coffee-mode")
-(require 'coffee-mode)
-(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
-(add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
