a88607ecf64b0a18ec4db720fb6bf76019d4d8aa
onjiro <onjiro.mohyahya@gmail.com>
2013-08-25 00:55:09 +0900

    Fix indent

--- .emacs.d/inits/00-settings.el
+++ .emacs.d/inits/00-settings.el
@@ -19,7 +19,7 @@
   "折り返し表示をトグル動作します."
   (interactive)
   (if truncate-lines
-      (setq truncate-lines nil)
+    (setq truncate-lines nil)
     (setq truncate-lines t))
   (recenter))
 (global-set-key "\C-c\C-l" 'toggle-truncate-lines)
