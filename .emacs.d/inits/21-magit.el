07a0435e0728d0c2bd44195d97474345a6fd4aa8
onjiro <onjiro.mohyahya@gmail.com>
2013-09-06 23:11:18 +0900

    Add magit window setting

--- .emacs.d/inits/21-magit.el
+++ .emacs.d/inits/21-magit.el
@@ -1,3 +1,20 @@
 (package-install-unless-installed 'magit)
 (require 'magit)
 (global-set-key (kbd "C-c C-SPC") 'magit-status)
+
+;; magit-statusを実行する前に Window構成を保存, その後最大化して
+;; magit-statusを開いて, セッション終了後, 保存した状態に戻す
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+
+(defun my/magit-quit-session ()
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :magit-fullscreen))
+
+(define-key magit-status-mode-map (kbd "q") 'my/magit-quit-session)
+
+(defadvice git-commit-commit (after move-to-magit-buffer activate)
+  (delete-window))
