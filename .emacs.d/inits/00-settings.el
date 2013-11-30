dbf8235a580da8a4caafd9e758b1a0c10b7e5633
onjiro <onjiro.mohyahya@gmail.com>
2013-12-01 00:50:37 +0900

    Record recentf using idle timer
    @see http://d.hatena.ne.jp/tomoya/20110217/1297928222

--- .emacs.d/inits/00-settings.el
+++ .emacs.d/inits/00-settings.el
@@ -21,3 +21,12 @@
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'post-forward-angle-brackets)
 (setq uniquify-ignore-buffers-re "*[^*]+*")
+
+;; recentf をバックエンドで記録
+(when (require 'recentf nil t)
+  (setq recentf-max-saved-items 2000)
+  (setq recentf-exclude '(".recentf"))
+  (setq recentf-auto-cleanup 10)
+  (setq recentf-auto-save-timer
+        (run-with-idle-timer 30 t 'recentf-save-list))
+  (recentf-mode 1))
