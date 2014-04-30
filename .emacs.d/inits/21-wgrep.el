b021d91651cbe29ab37eb1adae00bb2046b2505c
onjiro <onjiro.mohyahya@gmail.com>
2014-04-30 16:06:27 +0900

    Remove unnecessary empty lines

--- .emacs.d/inits/21-wgrep.el
+++ .emacs.d/inits/21-wgrep.el
@@ -1,14 +1,5 @@
-;; wgrep: make grep buffer writable
+?;; wgrep: make grep buffer writable
 ;;   start edit with 'C-c C-p'
 ;;   save with 'C-x C-s'
 (package-install-unless-installed 'wgrep)
 (require 'wgrep)
-
-
-
-
-
-
-
-
-
