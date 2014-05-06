3dd23ceb6dab870f0e03e830c0adf5bfdc0dcf52
onjiro <onjiro.mohyahya@gmail.com>
2014-05-06 22:41:19 +0900

    Delete unused function

--- .emacs.d/inits/00-keybind.el
+++ .emacs.d/inits/00-keybind.el
@@ -1,10 +1,3 @@
-;; capitalize を backword で適用
-(global-set-key "\M-C" 'capitalize-backward-word)
-(defun capitalize-backward-word (arg)
-  "Capitalize previous word (or arg words)."
-  (interactive "p")
-  (capitalize-word (- arg)))
-
 ;; C-c C-l で折り返しの on/off
 (global-set-key "\C-c\C-l" 'toggle-truncate-lines)
 
@@ -17,5 +10,6 @@
 ;; キーを連打することで何とかするための怠惰な設定
 (package-install-unless-installed 'key-combo)
 (require 'key-combo)
+(key-combo-load-default)
 (key-combo-define-global (kbd "-") '("-" " = " " == " " === "))
 (key-combo-define-global (kbd "[") '("[`!!']" "{`!!'}"))
