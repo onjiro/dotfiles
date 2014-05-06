63480db754b8f0be9b44ce5df2939af763cf682f
onjiro <onjiro.mohyahya@gmail.com>
2014-05-06 16:42:36 +0900

    Use yasnippet.el

---
+++ .emacs.d/inits/20-yasnippet.el
@@ -0,0 +1,35 @@
+;; @see http://konbu13.hatenablog.com/entry/2014/01/12/113300
+(package-install-unless-installed 'yasnippet)
+(require 'yasnippet)
+(yas-global-mode t)
+(define-key yas-minor-mode-map (kbd "M-i") 'yas-expand)
+
+(define-key (current-global-map) (kbd "C-x C-i") nil)
+;; 既存スニペットを挿入する
+(define-key yas-minor-mode-map (kbd "C-x i i") 'yas-insert-snippet)
+;; 新規スニペットを作成するバッファを用意する
+(define-key yas-minor-mode-map (kbd "C-x i n") 'yas-new-snippet)
+;; 既存スニペットを閲覧・編集する
+(define-key yas-minor-mode-map (kbd "C-x i v") 'yas-visit-snippet-file)
+
+;; helm interface
+;; http://d.hatena.ne.jp/syohex/20121207/1354885367
+(eval-after-load "helm"
+  '(progn
+     (defun my-yas/prompt (prompt choices &optional display-fn)
+       (let* ((names (loop for choice in choices
+                           collect (or (and display-fn (funcall display-fn choice))
+                                       choice)))
+              (selected (helm-other-buffer
+                         `(((name . ,(format "%s" prompt))
+                            (candidates . names)
+                            (action . (("Insert snippet" . (lambda (arg) arg))))))
+                         "*helm yas/prompt*")))
+         (if selected
+             (let ((n (position selected names :test 'equal)))
+               (nth n choices))
+           (signal 'quit "user quit!"))))
+     (custom-set-variables '(yas/prompt-functions '(my-yas/prompt)))))
+
+;; snippet-mode for *.yasnippet files
+(add-to-list 'auto-mode-alist '("\\.yasnippet$" . snippet-mode))
