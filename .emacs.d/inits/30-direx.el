00f1dbaa8546a3d9b67d10ae96eee868f4386066
onjiro <onjiro.mohyahya@gmail.com>
2014-05-07 23:39:52 +0900

    Rename direx.el not to override key bind "C-x C-j"

--- .emacs.d/inits/30-direx.el
+++ .emacs.d/inits/30-direx.el
@@ -1,7 +1,6 @@
 ;; @see http://cx4a.blogspot.jp/2011/12/popwineldirexel.html
 (package-install-unless-installed 'direx)
 (require 'direx)
-(global-set-key (kbd "C-x C-j") 'direx:jump-to-directory)
 
 (push '(direx:direx-mode :position left :width 50 :dedicated t)
       popwin:special-display-config)
