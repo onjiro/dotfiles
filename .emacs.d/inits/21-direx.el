94980e5aa490ef98205b6044622d35a0e82885d3
onjiro <onjiro.mohyahya@gmail.com>
2014-05-07 22:23:19 +0900

    Widen direx window

--- .emacs.d/inits/21-direx.el
+++ .emacs.d/inits/21-direx.el
@@ -3,6 +3,7 @@
 (require 'direx)
 (global-set-key (kbd "C-x C-j") 'direx:jump-to-directory)
 
-(push '(direx:direx-mode :position left :width 25 :dedicated t)
+(push '(direx:direx-mode :position left :width 50 :dedicated t)
       popwin:special-display-config)
+
 (global-set-key (kbd "C-x C-j") 'direx:jump-to-directory-other-window)
