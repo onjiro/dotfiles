e477d251152b949ff30541821a1c1a3f8acaa3f5
onjiro <onjiro.mohyahya@gmail.com>
2014-05-07 23:35:48 +0900

    Fix key-bind using key-combo

--- .emacs.d/inits/00-keybind.el
+++ .emacs.d/inits/00-keybind.el
@@ -10,6 +10,10 @@
 ;; キーを連打することで何とかするための怠惰な設定
 (package-install-unless-installed 'key-combo)
 (require 'key-combo)
-(key-combo-load-default)
+(global-key-combo-mode 1)
 (key-combo-define-global (kbd "-") '("-" " = " " == " " === "))
 (key-combo-define-global (kbd "[") '("[`!!']" "{`!!'}"))
+(key-combo-define-global (kbd "2") '("2" "\"`!!'\"" "22" "222" "2222" "22222" "222222" "2222222" "22222222" "222222222"))
+(key-combo-define-global (kbd "7") '("7" "'`!!''" "77" "777" "7777" "77777" "777777" "7777777" "77777777" "777777777"))
+(key-combo-define-global (kbd "8") '("8" "(`!!')" "88" "888" "8888" "88888" "888888" "8888888" "88888888" "888888888"))
+(key-combo-define-global (kbd "\\") '("\\" "_"))
