9ef1a8f0a953028842d2e37c43799e68d8715c01
onjiro <onjiro.mohyahya@gmail.com>
2014-06-02 20:32:33 +0900

    Set key-combo on coffee-mode

--- .emacs.d/inits/21-coffee-mode.el
+++ .emacs.d/inits/21-coffee-mode.el
@@ -2,4 +2,7 @@
 (require 'coffee-mode)
 (add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
 (add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
-
+(add-hook 'coffee-mode-hook
+          (lambda ()
+            (key-combo-define-local (kbd "-") '("-" "->"))
+            ))
