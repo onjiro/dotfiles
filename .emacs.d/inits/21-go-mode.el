6080a89336ddfa3efc3ca5fc8577596b0730f133
onjiro <onjiro.mohyahya@gmail.com>
2014-07-03 11:18:23 +0900

    Add key-combo setting for "::"

--- .emacs.d/inits/21-go-mode.el
+++ .emacs.d/inits/21-go-mode.el
@@ -1,7 +1,12 @@
 (package-install-unless-installed 'go-mode)
 
 (require 'go-mode)
-(add-hook 'before-save-hook 'gofmt-before-save)
 
 ;;; exclude go file from auto-save-buffers-enhanced
 (push ".+\.go$" auto-save-buffers-enhanced-exclude-regexps)
+
+(add-hook 'before-save-hook 'gofmt-before-save)
+(add-hook 'go-mode-hook
+          (lambda ()
+            (key-combo-define-local (kbd ":") '(":" " := " "::"))
+            ))
