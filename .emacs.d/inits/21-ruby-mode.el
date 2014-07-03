8ffd2b36f81e22367c7d06a9421d674e5adaedca
onjiro <onjiro.mohyahya@gmail.com>
2014-07-03 11:23:34 +0900

    Use ruby-mode on Gemfile, Rakefile, Capfile

--- .emacs.d/inits/21-ruby-mode.el
+++ .emacs.d/inits/21-ruby-mode.el
@@ -7,8 +7,10 @@
 
 (autoload 'ruby-mode "ruby-mode"
   "Mode for editing ruby source files" t)
-(setq auto-mode-alist
-      (append '(("\\.rb$" . ruby-mode)) auto-mode-alist))
+(add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile" . ruby-mode))
 (setq interpreter-mode-alist (append '(("ruby" . ruby-mode))
                                      interpreter-mode-alist))
 (autoload 'run-ruby "inf-ruby"
