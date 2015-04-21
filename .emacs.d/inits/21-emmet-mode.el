6ce78867aae5edfe118a4f29047300fdd369202d
onjiro <onjiro.mohyahya@gmail.com>
2015-04-21 18:55:19 +0900

    Add web-mode

--- .emacs.d/inits/21-emmet-mode.el
+++ .emacs.d/inits/21-emmet-mode.el
@@ -1,12 +1,3 @@
 (require 'emmet-mode)
 (add-hook 'html-mode-hook 'emmet-mode)
-
-
-
-
-
-
-
-
-
-
+(add-hook 'web-mode-hook 'emmet-mode)
--- .emacs.d/inits/21-html-mode.el
+++ .emacs.d/inits/21-html-mode.el
@@ -1,4 +1,3 @@
-(add-to-list 'auto-mode-alist '("\\.html.erb$" . html-mode))
 (add-hook 'html-mode-hook
           (lambda()
             (autopair-mode 0)
