912bb4ff75947b7043a1ace75095d380af1cc951
onjiro <onjiro.mohyahya@gmail.com>
2016-03-14 22:24:05 +0900

    カーソル移動速度の対策

--- .emacs.d/inits/00-settings.el
+++ .emacs.d/inits/00-settings.el
@@ -29,6 +29,9 @@
 (global-auto-revert-mode t)
 
 ;; 現在行をハイライト
+;; カーソル移動が重くなる対策
+;; http://rubikitch.com/2015/05/14/global-hl-line-mode-timer/
+(require 'hl-line)
 (defface hlline-face
   '((((class color)
       (background dark))
@@ -40,11 +43,39 @@
      ()))
   "*Face used by hl-line.")
 (setq hl-line-face 'hlline-face)
-(global-hl-line-mode)
+(defun global-hl-line-timer-function ()
+  (global-hl-line-unhighlight-all)
+  (let ((global-hl-line-mode t))
+    (global-hl-line-highlight)))
+(setq global-hl-line-timer
+      (run-with-idle-timer 0.03 t 'global-hl-line-timer-function))
+;; (cancel-timer global-hl-line-timer)
 
 ;; dired からファイル名変更を可能に
 (require 'wdired)
 (define-key dired-mode-map "r" 'wdired-change-to-wdired-mode)
 
 ;; タブの表示幅を4に
-(setq default-tab-width 4)
+(setq tab-width 4)
+
+;; system-type predicates
+;; from http://d.hatena.ne.jp/tomoya/20090807/1249601308
+(setq darwin-p   (eq system-type 'darwin)
+      linux-p    (eq system-type 'gnu/linux)
+      carbon-p   (eq system-type 'mac)
+      meadow-p   (featurep 'meadow))
+
+;; Emacs と Mac のクリップボード共有
+;; from http://hakurei-shain.blogspot.com/2010/05/mac.html
+(defun copy-from-osx ()
+  (shell-command-to-string "pbpaste"))
+
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+
+(if (or darwin-p carbon-p)
+    (setq interprogram-cut-function 'paste-to-osx)
+    (setq interprogram-paste-function 'copy-from-osx))
