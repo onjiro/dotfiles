0c2752f658886d19acccaf8796c796445a730f14
onjiro <onjiro.mohyahya@gmail.com>
2016-05-04 23:25:34 +0900

    magit を 2 系にバージョンアップ

--- .emacs.d/inits/50-vcs.el
+++ .emacs.d/inits/50-vcs.el
@@ -4,31 +4,26 @@
 (el-get-bundle 'git-gutter)
 (global-git-gutter-mode +1)
 
-;; バージョン2以降ではキーバインドがまだ安定していないためしばらく1系を使う
-(el-get-bundle 'magit/git-modes
-  :features ("git-commit-mode" "git-rebase-mode" "gitattributes-mode" "gitconfig-mode" "gitignore-mode")
-  :branch "1.0.0")
-(el-get-bundle! 'magit/magit
-  :depends (git-modes)
-  :branch "1.4.2")
-(setq magit-last-seen-setup-instructions "1.4.0")
+(el-get-bundle magit/with-editor
+  :depends (dash))
+(el-get-bundle magit)
 (global-set-key (kbd "C-c C-SPC") 'magit-status)
 
 ;; magit-statusを実行する前に Window構成を保存, その後最大化して
 ;; magit-statusを開いて, セッション終了後, 保存した状態に戻す
-(defadvice magit-status (around magit-fullscreen activate)
-  (window-configuration-to-register :magit-fullscreen)
-  ad-do-it
-  (delete-other-windows))
-
-(defun my/magit-quit-session ()
-  (interactive)
-  (kill-buffer)
-  (jump-to-register :magit-fullscreen))
-
-(define-key magit-status-mode-map (kbd "q") 'my/magit-quit-session)
-
-(defadvice git-commit-commit (after move-to-magit-buffer activate)
-  (delete-window))
-
-(add-hook 'git-commit-mode-hook (setq auto-fill-mode nil))
+;; (defadvice magit-status (around magit-fullscreen activate)
+;;   (window-configuration-to-register :magit-fullscreen)
+;;   ad-do-it
+;;   (delete-other-windows))
+;; 
+;; (defun my/magit-quit-session ()
+;;   (interactive)
+;;   (kill-buffer)
+;;   (jump-to-register :magit-fullscreen))
+;; 
+;; (define-key magit-status-mode-map (kbd "q") 'my/magit-quit-session)
+;; 
+;; (defadvice git-commit-commit (after move-to-magit-buffer activate)
+;;   (delete-window))
+;; 
+;; (add-hook 'git-commit-mode-hook (setq auto-fill-mode nil))
