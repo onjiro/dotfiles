61b4c647c65816468c88d5100751ec9cc9cf543d
onjiro <onjiro.mohyahya@gmail.com>
2016-07-24 22:38:57 +0900

    update emacs lisps

--- .emacs.d/inits/21-helm.el
+++ .emacs.d/inits/21-helm.el
@@ -12,10 +12,11 @@
   "*my-helm-mini*"))
 
 (helm-mode 1)
-(global-set-key "\C-x\C-f" 'my-helm-mini+)
-(global-set-key "\C-x\C-v" 'find-file)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x C-f") 'my-helm-mini+)
+(global-set-key (kbd "M-u C-f") 'find-FILE)
 
-(setq helm-buffer-max-length 50)
+(custom-set-variables '(helm-buffer-max-length 50))
 (setq recentf-max-menu-items 200)
 (setq recentf-max-saved-items 3000)
 
