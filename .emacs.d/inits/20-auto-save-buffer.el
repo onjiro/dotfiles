0807143e11b18fc56875bf97529d7c40b3090dd3
onjiro <onjiro.mohyahya@gmail.com>
2018-02-17 14:37:00 +0900

    remove auto-save-buffer-enhanced

--- .emacs.d/inits/20-auto-save-buffer.el
+++
@@ -1,4 +0,0 @@
-(el-get-bundle! 'kentaro/auto-save-buffers-enhanced)
-(auto-save-buffers-enhanced t)
-(setq auto-save-buffers-enhanced-include-regexps '(".+"))
-(setq auto-save-buffers-enhanced-exclude-regexps '("^not-save-file" "\\.ignore$"))
