777e565a77aab3dc1c85f5f16ec212d1a92f885f
onjiro <onjiro.mohyahya@gmail.com>
2018-07-25 23:16:41 +0900

    improve recent-f timing

--- .emacs.d/inits/20-recentf.el
+++ .emacs.d/inits/20-recentf.el
@@ -2,9 +2,9 @@
 (when (require 'recentf nil t)
   (setq recentf-max-saved-items 2000)
   (setq recentf-exclude '(".recentf"))
-  (setq recentf-auto-cleanup 10)
+  (setq recentf-auto-cleanup 'never)
   (setq recentf-auto-save-timer
-        (run-with-idle-timer 60 t 'recentf-save-list))
+        (run-with-idle-timer 300 t 'recentf-save-list))
   (recentf-mode 1))
 
 ;; recentf で dired も記録する
